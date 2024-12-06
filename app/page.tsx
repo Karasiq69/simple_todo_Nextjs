a16978a8e6a3bd8ab8ac8085b70617fd23cc9429
karasiq69 <0982646@mail.ru>
2024-12-06 18:03:28 +0300

    added git links

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,53 @@
 import TodosView from "@/components/TodosView";
 import AddTodo from "@/components/AddTodo";
+import {Button} from "@/components/ui/button";
+import {FolderRoot, Github, MonitorCog, UserIcon} from "lucide-react";
+import Link from "next/link";
 
+const links = [
+    {
+        title: 'backend git',
+        url: 'https://github.com/Karasiq69/nestapp_todo',
+        icon: <FolderRoot />,
+    },
+    {
+        title: 'frontend git',
+        url: 'https://github.com/Karasiq69/simple_todo_Nextjs',
+        icon: <FolderRoot />,
+    },
+    {
+        title: 'nestjs backend api',
+        url: 'https://nestjs.pmercedes.ru/todo',
+        icon: <MonitorCog />,
+    },
+    {
+        title: 'Resume (hh.ru)',
+        url: 'https://hh.ru/resume/c295d910ff0da545b60039ed1f6a676f506565',
+        icon: <UserIcon/>,
+    },
 
+]
 export default function Home() {
 
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px]   justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <div>
+                 {links.map((link) => {
+                    return (
+                        <Button key={link.title} variant={'link'} className={'gap-2'} asChild>
+                            <Link href={link.url} target={'_blank'}>
+                                {link.icon && link.icon}
+                            {link.title}
+                            </Link>
+
+                        </Button>
+                    )
+                })}
+             </div>
+
             <main className="flex flex-col gap-8 row-start-2   sm:items-start bg-secondary p-20">
+
                 <div className={'flex gap-3 w-full max-w-md'}>
                     <AddTodo/>
                 </div>
