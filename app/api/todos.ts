8a8291f1583affd890366647d73a264ccac84c16
karasiq69 <0982646@mail.ru>
2024-12-06 17:20:06 +0300

    Added some components

--- app/api/todos.ts
+++ app/api/todos.ts
@@ -5,6 +5,7 @@ import {Todo} from "@/types/todo";
 
 export const getTodos = async () => {
     const response = await axiosInstance.get<Todo[]>('/todo')
+    console.log('TODOS:',response.data)
     return response.data
 }
 
